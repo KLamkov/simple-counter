0e8ca021d63f5c853879291815f00cca88583a61
KLamkov <kirilllamkov@gmail.com>
2022-11-12 09:46:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	const onClickPlus = () => {
+		setCount(count + 1)
+	}
+
+	const onClickRandom = () => {
+		setCount(Math.floor(Math.random() * 100))
+	}
+
+	const onClickMinus = () => {
+		setCount(count - 1)
+	}
+
+	return (
+		<div className="App">
+		<div>
+			<h2>Counter:</h2>
+			<h1>{count}</h1>
+			<button onClick={onClickMinus} className="minus">
+				- Minus
+			</button>
+			<button onClick={onClickRandom} className="random">
+				Random 
+			</button>
+			<button onClick={onClickPlus} className="plus">
+				Plus +
+			</button>
+		</div>
+		</div>
+	);
 }
 
 export default App;
